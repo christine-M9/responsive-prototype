6db47a131e8d2ee84c6ad27cee7f698ef2c3387a
Christine <christinekhekama99@gmail.com>
2023-12-17 19:46:07 +0300

    landingpage styling

--- client/src/LandingPage.tsx
+++ client/src/LandingPage.tsx
@@ -1,4 +1,3 @@
-// LandingPage.tsx
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -10,16 +9,16 @@ const LandingPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <div>
-        <Link to="/user-management" className="landing-page-card">
-          <div className="card-content">
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif', maxWidth: '600px', margin: 'auto' }}>
+      <h1 style={{ marginBottom: '20px', color: '#333', textAlign: 'center' }}>Landing Page</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+        <Link to="/user-management" className="landing-page-card" style={{ textDecoration: 'none' }}>
+          <div className="card-content" style={{ backgroundColor: '#007bff', color: '#fff', padding: '20px', borderRadius: '8px', textAlign: 'center', cursor: 'pointer' }}>
             <h3>User Management</h3>
           </div>
         </Link>
-        <button className="landing-page-card hover-effect" onClick={handleServiceAssuranceClick}>
-          <div className="card-content hover-effect">
+        <button className="landing-page-card hover-effect" onClick={handleServiceAssuranceClick} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
+          <div className="card-content hover-effect" style={{ backgroundColor: '#28a745', color: '#fff', padding: '20px', borderRadius: '8px', textAlign: 'center' }}>
             <h3>Service Assurance</h3>
           </div>
         </button>
