23e706b1ebbb98e3e81a9aa9d465a3baccb71315
Christine <christinekhekama99@gmail.com>
2023-12-17 14:12:32 +0300

    hoover on assuarance view

--- prototype/src/LandingPage.tsx
+++ prototype/src/LandingPage.tsx
@@ -1,18 +1,23 @@
+// LandingPage.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './index.css'; // Import the CSS file
 
 const LandingPage: React.FC = () => {
   return (
     <div>
       <h1>Landing Page</h1>
       <div>
-        <Link to="/user-management" style={{ textDecoration: 'none', padding: '16px', margin: '8px', textAlign: 'center', cursor: 'pointer', transition: 'background-color 0.3s' }}>
-          <div style={{ backgroundColor: '#fff', borderRadius: '8px', padding: '16px' }}>
+        {/* User Management Card */}
+        <Link to="/user-management" className="landing-page-card">
+          <div className="card-content">
             <h3>User Management</h3>
           </div>
         </Link>
-        <Link to="/assurance" style={{ textDecoration: 'none', padding: '16px', margin: '8px', textAlign: 'center', cursor: 'pointer', transition: 'background-color 0.3s' }}>
-          <div style={{ backgroundColor: '#fff', borderRadius: '8px', padding: '16px' }}>
+
+        {/* Service Assurance Card with Hover Effect */}
+        <Link to="/assurance" className="landing-page-card hover-effect">
+          <div className="card-content hover-effect">
             <h3>Service Assurance</h3>
           </div>
         </Link>
