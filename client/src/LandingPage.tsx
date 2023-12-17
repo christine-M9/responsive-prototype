0c3b589c2c126280b7f6b5b5dd8bb6c775612ca3
Christine <christinekhekama99@gmail.com>
2023-12-17 20:15:45 +0300

    styling of the landpage

--- client/src/LandingPage.tsx
+++ client/src/LandingPage.tsx
@@ -9,20 +9,21 @@ const LandingPage: React.FC = () => {
   };
 
   return (
-    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif', maxWidth: '600px', margin: 'auto' }}>
-      <h1 style={{ marginBottom: '20px', color: '#333', textAlign: 'center' }}>Landing Page</h1>
+    <div style={{ padding: '40px', fontFamily: 'Arial, sans-serif', maxWidth: '800px', margin: 'auto', background: '#f8f9fa', borderRadius: '8px' }}>
+      <h1 style={{ marginBottom: '30px', color: '#007bff', textAlign: 'center', fontSize: '2.5em' }}>RESPONSIVE PROTOTYPE</h1>
       <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
         <Link to="/user-management" className="landing-page-card" style={{ textDecoration: 'none' }}>
-          <div className="card-content" style={{ backgroundColor: '#007bff', color: '#fff', padding: '20px', borderRadius: '8px', textAlign: 'center', cursor: 'pointer' }}>
+          <div className="card-content" style={{ backgroundColor: '#007bff', color: '#fff', padding: '30px', borderRadius: '10px', textAlign: 'center', cursor: 'pointer', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
             <h3>User Management</h3>
+            <p style={{ marginTop: '15px' }}>Effortlessly manage users and groups with our intuitive tools.</p>
           </div>
         </Link>
         <button className="landing-page-card hover-effect" onClick={handleServiceAssuranceClick} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
-          <div className="card-content hover-effect" style={{ backgroundColor: '#28a745', color: '#fff', padding: '20px', borderRadius: '8px', textAlign: 'center' }}>
+          <div className="card-content hover-effect" style={{ backgroundColor: '#28a745', color: '#fff', padding: '30px', borderRadius: '10px', textAlign: 'center', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
             <h3>Service Assurance</h3>
+            <p style={{ marginTop: '15px' }}>Ensure the reliability and quality of your services with our advanced assurance features.</p>
           </div>
         </button>
-        {/* Add other links as needed */}
       </div>
     </div>
   );
