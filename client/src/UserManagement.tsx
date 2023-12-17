dbf79b01a5b66c05dfe900d4d8c603c9d7557e93
Christine <christinekhekama99@gmail.com>
2023-12-17 19:00:26 +0300

    user management

--- client/src/UserManagement.tsx
+++ client/src/UserManagement.tsx
@@ -1,15 +1,62 @@
 // UserManagement.tsx
-
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+interface User {
+  id: string;
+  name: string;
+}
 
+interface UserGroup {
+  id: string;
+  groupName: string;
+  members: User[];
+}
+
+const users: User[] = [
+  { id: '1', name: 'John Doe' },
+  { id: '2', name: 'Jane Smith' },
+  // Add more users as needed
+];
+
+const userGroups: UserGroup[] = [
+  { id: 'group1', groupName: 'Group A', members: [users[0], users[1]] },
+  { id: 'group2', groupName: 'Group B', members: [users[1] /* Add more members as needed */] },
+  // Add more user groups as needed
+];
 
 const UserManagement: React.FC = () => {
-  // Implement User Management UI
   return (
     <div>
       <h1>User Management</h1>
-      {/* Add user and user group lists */}
+
+      <div>
+        <h2>Users</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h2>User Groups</h2>
+        <ul>
+          {userGroups.map((group) => (
+            <li key={group.id}>
+              <strong>{group.groupName}</strong>
+              <ul>
+                {group.members.map((member) => (
+                  <li key={member.id}>{member.name}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Add a link to navigate back to the landing page */}
+      <Link to="/">BACK</Link>
     </div>
   );
 };
